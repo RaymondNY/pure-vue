c6211e4ecf7dc64c8d1bfec4f07c79eb89fbc6af
zhangqixuan <zhangqixuan@qq.com>
2021-11-05 16:27:42 +0800

    anniu

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="user">
-    <hy-form v-bind="searchFormConfig" />
+    <hy-form v-bind="searchFormConfig" v-model="formData">
+      <template #header>
+        <h1 class="header">高级检索</h1>
+      </template>
+      <template #footer>
+        <div class="handle-btns">
+          <el-button type="primary" icon="el-icon-refresh">reset</el-button>
+          <el-button type="primary" icon="el-icon-refresh">search</el-button>
+        </div>
+      </template>
+    </hy-form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import HyForm from '@/base-ui/form'
 import { searchFormConfig } from './config/search.config'
 export default defineComponent({
@@ -14,9 +24,24 @@ export default defineComponent({
     HyForm
   },
   setup() {
-    return { searchFormConfig }
+    const formData = ref({
+      id: '',
+      name: '',
+      password: '',
+      sport: '',
+      createTime: ''
+    })
+    return { searchFormConfig, formData }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.header {
+  color: red;
+}
+.handle-btns {
+  text-align: right;
+  padding: 0 50px 20px 0;
+}
+</style>
